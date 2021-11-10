39e78da1deda2d16409b052ac244fe834648aa94
Zacharie Makeen <zach.makeen@gmail.com>
2021-11-10 10:25:40 -0500

    Test new features in load module

--- server/__tests__/load.test.js
+++ server/__tests__/load.test.js
@@ -2,32 +2,40 @@
 const load = require("../utils/load.js");
 const { expect } = require("@jest/globals");
 
-test("file is found and parsed sucessfully", async () => {
-  const expected = ["This is a sample json file."];
-  const data = await load("./dataset/meteorite-landings.json");
-  // expect(data["test"][0]).toMatch(expected[0]);
+test("lat and long properties are defined", async () => {
+  const expected = [6.08333, 50.775];
+  const data = await load("./__tests__/sample.json");
+  console.log(data)
+  expect(data[0].geo.coordinates[0]).toBe(expected[0]);
 });
 
-// test("file is found and parsed sucessfully", async () => {
-//   const expected = ["This is a sample json file."];
-//   const data = await load("./__tests__/sample.json");
-//   expect(data["test"][0]).toMatch(expected[0]);
-// });
+test("lat and long properties are undefined", async () => {
+  const expected = "Nullarbor 002";
+  const data = await load("./__tests__/sample-undefined-lat-long.json");
+  console.log(data)
+  expect(data[0].name).not.toMatch(expected);
+});
 
-// test("file is not in JSON format", async () => {
-//   expect.assertions(1);
-//   try {
-//     await load("./__tests__/broken.json");
-//   } catch (e) {
-//     expect(e.message).toMatch("Unexpected");
-//   }
-// });
+test("file is found and parsed sucessfully", async () => {
+  const expected = ["Aachen"];
+  const data = await load("./__tests__/sample.json");
+  expect(data[0]["name"]).toMatch(expected[0]);
+});
 
-// test("file is not found", async () => {
-//   expect.assertions(1);
-//   try {
-//     await load("./__tests__/non-existant.json");
-//   } catch (e) {
-//     expect(e.message).toMatch("no such");
-//   }
-// });
+test("file is not in JSON format", async () => {
+  expect.assertions(1);
+  try {
+    await load("./__tests__/broken.json");
+  } catch (e) {
+    expect(e.message).toMatch("Unexpected");
+  }
+});
+
+test("file is not found", async () => {
+  expect.assertions(1);
+  try {
+    await load("./__tests__/non-existant.json");
+  } catch (e) {
+    expect(e.message).toMatch("no such");
+  }
+});
