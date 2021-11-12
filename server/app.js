63036fd7d573c44ea2652b2dbe4ac4197014d8cc
Zacharie Makeen <zach.makeen@gmail.com>
2021-11-12 16:17:17 -0500

    Create an API specification

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API for JSONPlaceholder",
+    version: "1.0.0",
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
