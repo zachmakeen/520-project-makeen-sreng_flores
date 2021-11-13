3835133c2c1fe73c1b08d4837c05e9ebfc80ea1c
Zacharie Makeen <zach.makeen@gmail.com>
2021-11-13 16:03:39 -0500

    Create an endpoint that returns all the documents whose points are found within a given rectangle

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,11 +7,26 @@ const db = new DAO();
 //parser middleware will parse the json payload
 router.use(express.json());
 
+//retrieve a list of meteorite landings
 router.get("/", async (req, res) => {
-  // console.log(req.body);
-  // res.sendStatus(201).end();
   try {
     let ml = await db.findAll();
+    res.send(ml);
+  } catch (e) {
+    console.error(e.message);
+    res.sendStatus(500).end();
+  }
+});
+
+//retrieve a list of meteorite landings
+router.get("/meteorite_landings", async (req, res) => {
+  try {
+    let ml = await db.findAllInRectangle(
+      req.query.neLat,
+      req.query.neLon,
+      req.query.swLat,
+      req.query.swLon
+    );
     res.send(ml);
   } catch (e) {
     console.error(e.message);
