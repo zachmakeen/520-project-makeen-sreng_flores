bbd271d211bc39a8f9851d15705e91b378997b7a
Study Buggy <workaccount@Juan-Carloss-MacBook-Pro.local>
2021-11-15 22:31:19 -0500

    Saving changes

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -1,11 +1,24 @@
 const request = require("supertest");
 const app = require("../app")
-const { DAO } = require("../db/conn")
+const { DAO } = require("../db/conn");
 
-describe("GET /api ", async () => {
-  jest.spyOn(DAO.prototype, "findAll").mockResolvedValue({ "_id": "randomId" })
-  const response = await request(app).
-    get("/api");
-  console.log(response.text)
-  console.log(response.statusCode)
+jest.mock("../db/conn");
+
+describe("GET /api ", () => {
+  test("It should respond", async () => {
+    const resolvedValue = [{ "_id": "randomId" }]
+    jest.spyOn(DAO.prototype, "findAll").mockResolvedValue(resolvedValue)
+    const response = await request(app).get("/api");
+    console.log(response.text)
+    expect(response.text).toEqual(JSON.stringify(resolvedValue))
+    expect(response.statusCode).toBe(200)
+    expect(response.type).toBe("application/json")
+  })
+})
+
+describe("GET /api/", () => {
+  test("It should respond", async () => {
+    const resvolved = []
+    jest.spyOn(DAO.prototype)
+  })
 })
