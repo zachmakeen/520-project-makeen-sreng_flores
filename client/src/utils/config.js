c7c5cc90be027dfc4eddfd38bf72aeb0023ad983
Juan-Carlos Sreng-Flores <juan-carlos.sreng-flores@dawsoncollege.qc.ca>
2021-12-06 21:40:58 -0500

    Fix eslint error to fix pipeline

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,7 +3,9 @@
 const attribution = "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors";
 
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-
-let minZoom; // to be changed to a const later
-let maxZoom; // to be changed to a const later
-let initialZoom; // to be changed to a const later
+// to be changed to a const later
+let minZoom;
+// to be changed to a const later
+let maxZoom;
+// to be changed to a const later
+let initialZoom;
