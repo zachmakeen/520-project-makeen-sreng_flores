6069c4e08d1b36dda99b1762bb47dd6e4f6a7fba
Juan-Carlos Sreng-Flores <juan-carlos.sreng-flores@dawsoncollege.qc.ca>
2021-12-06 20:11:11 -0500

    Saving changes setup some files for implementation

---
+++ client/src/components/MeteoriteMapApp.js
@@ -0,0 +1,12 @@
+
+import { Component } from "react";
+class MeteoriteMapApp extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // Just for testing
+    return <p>Hello</p>;
+  }
+}
+export default MeteoriteMapApp;
---
+++ client/src/utils/config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const attribution = "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors";
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+let minZoom; // to be changed to a const later
+let maxZoom; // to be changed to a const later
+let initialZoom; // to be changed to a const later
