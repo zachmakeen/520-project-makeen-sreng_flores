cb7f7e2277f5061c1eb569c72b4be946d4bf71f1
Juan-Carlos Sreng-Flores <juan-carlos.sreng-flores@dawsoncollege.qc.ca>
2021-12-10 05:41:14 -0500

    Provide attribution to source of informations

--- client/src/components/MeteoriteMapMove.js
+++ client/src/components/MeteoriteMapMove.js
@@ -1,9 +1,22 @@
-import { 
+/**
+ * The following snippet of code was taken from the author below, 
+ * it performs a notification system where the parent component may be notified 
+ * if the user changes the bounds of the map.
+ * @author Jaya Nikalantan
+ */
+import {
   useMapEvents,
 } from "react-leaflet";
-  
-//react-leaflet hook
-function MeteoriteMapMove(props) {    
+
+/**
+ * This function uses the useMapsEvents in order to receive notification once the user has changes
+ * bounds in the map. Upon changing the bounds, the callback function will be called to notify 
+ * a parent component for rerendering the app.
+ * Attribution to Professor at Dawson College Jaya Nilakantan for the component!
+ * @param {*} props 
+ * @returns {null} 
+ */
+function MeteoriteMapMove(props) {
   const mapEvents = useMapEvents({
     "moveend": () => {
       props.action(mapEvents.getBounds());
@@ -11,9 +24,8 @@ function MeteoriteMapMove(props) {
     "zoom": () => {
       props.action(mapEvents.getBounds());
     },
-  }); 
-  return null 
-} 
-  
+  });
+  return null;
+}
+
 export default MeteoriteMapMove;
-  
